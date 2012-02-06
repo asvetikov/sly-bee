05b0fc8bdb3d6ae7b979d3104f09c1eef7e0451d
pinocchio964 <pelou85@gmail.com>
2012-02-06 23:53:03 +0400

    first commit

---
+++ controllers/acts.js
@@ -0,0 +1,22 @@
+//
+// Acts controller
+//
+
+
+module.exports = {
+    // actions
+    indexAction: function () {
+    },
+    signAction: function () {
+    },
+    generateAction: function() {
+    },
+    agentsAction: function() {
+    },
+    // public functions
+    getActs: function() {
+    },
+    getAgentsActs: function(id) {
+    }
+};
+
